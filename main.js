1ba699abad607d7653665904fcbbcf8e8fa2fcae
Sziro06 <huoea071@gmail.com>
2023-12-14 18:59:06 +0100

    Uploading old files

---
+++ main.js
@@ -0,0 +1,14 @@
+let countDown = document.getElementById('countdown');
+let wholePage = document.getElementById('whole_page');
+
+wholePage.style.display =  'none';
+
+
+countDown.width = window.innerWidth;
+
+countDown.addEventListener("ended", function() {
+
+    countDown.style.display = 'none';
+    wholePage.style.display =  'block';
+
+})
