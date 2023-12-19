caf9b9873848d881f056771dd03ed59d2654fc31
Sziro06 <huoea071@gmail.com>
2023-12-19 20:03:34 +0100

    Trying something new

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let wholePage = document.getElementById('whole_page');
 // wholePage.style.display =  'none';
 
 
-countDown.width = window.innerWidth;
+//countDown.width = window.innerWidth;
 
-countDown.addEventListener("ended", function() {
+// countDown.addEventListener("ended", function() {
 
-    countDown.style.display = 'none';
-    wholePage.style.display =  'block';
+//     countDown.style.display = 'none';
+//     wholePage.style.display =  'block';
 
-})
+// })
