d54803736098f7acf9223a22495d91967eb1c5f8
Sziro06 <huoea071@gmail.com>
2024-01-20 16:37:13 +0100

    Trying to do something new

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 let countDown = document.getElementById('countdown');
 let wholePage = document.getElementById('whole_page');
+let body = document.querySelector('body')
 
-// wholePage.style.display =  'none';
+wholePage.style.display =  'none';
 
 
-//countDown.width = window.innerWidth;
+countDown.width = window.innerWidth;
 
-// countDown.addEventListener("ended", function() {
+countDown.addEventListener("ended", function() {
 
-//     countDown.style.display = 'none';
-//     wholePage.style.display =  'block';
+    countDown.style.display = 'none';
+    wholePage.style.display =  'block';
+    wholePage.style.backgroundColor = 'rgb(135, 206, 235)';
+    body.style.backgroundColor = 'rgb(135, 206, 235)';
 
-// })
+})
