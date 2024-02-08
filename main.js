a097cd6351cea88837d0eebdaca24739421bdda4
Sziro06 <huoea071@gmail.com>
2024-02-08 16:52:38 +0100

    Fixing

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ let countDown = document.getElementById('countdown');
 let wholePage = document.getElementById('whole_page');
 let body = document.querySelector('body')
 
-wholePage.style.display =  'none';
+// wholePage.style.display =  'none';
 
 
-countDown.width = window.innerWidth;
+// countDown.width = window.innerWidth;
 
-countDown.addEventListener("ended", function() {
+// countDown.addEventListener("ended", function() {
 
-    countDown.style.display = 'none';
-    wholePage.style.display =  'block';
-    wholePage.style.backgroundColor = 'rgb(135, 206, 235)';
-    body.style.backgroundColor = 'rgb(135, 206, 235)';
+//     countDown.style.display = 'none';
+//     wholePage.style.display =  'block';
+//     wholePage.style.backgroundColor = 'rgb(135, 206, 235)';
+//     body.style.backgroundColor = 'rgb(135, 206, 235)';
 
-})
+// })
